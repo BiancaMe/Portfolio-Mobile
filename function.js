ec0e617d675ec3767b35d3ca7e73cc1e926f48c2
BiancaMe <bianquis0410@gmail.com>
2023-06-26 12:14:48 -0300

    Update function js file

--- function.js
+++ function.js
@@ -1,7 +1,11 @@
 const action=document.querySelector('#menu-action');
 const menu=document.querySelector('#menu-mobile');
+const close=document.querySelector('.close-menu');
 
 action.addEventListener('click', function(){
-    console.log(action);
     menu.classList.add('menu-open');
+});
+
+close.addEventListener('click', function(){
+    menu.classList.remove('menu-open');
 });
