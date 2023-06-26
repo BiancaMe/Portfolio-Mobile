33aedcaa8fb92098f7f93f5d41800aaad7086ff9
BiancaMe <bianquis0410@gmail.com>
2023-06-26 15:41:51 -0300

    Update styles file, funcition.js and Index. Update linters

--- function.js
+++ function.js
@@ -1,11 +1,36 @@
 const action=document.querySelector('#menu-action');
 const menu=document.querySelector('#menu-mobile');
 const close=document.querySelector('.close-menu');
+const headline=document.querySelector('#headline-link');
+const myself=document.querySelector('#myself-link');
+const contact=document.querySelector('#contact-link');
 
-action.addEventListener('click', function(){
+
+action.addEventListener('click', function(event){
     menu.classList.add('menu-open');
+    event.preventDefault();
 });
 
-close.addEventListener('click', function(){
+close.addEventListener('click', function(event){
     menu.classList.remove('menu-open');
+    event.preventDefault();
 });
+
+headline.addEventListener('click', function(event){
+    menu.classList.remove('menu-open');
+    window.location.hash='#headline';
+    event.preventDefault();
+});
+
+myself.addEventListener('click', function(event){
+    menu.classList.remove('menu-open');
+    window.location.hash='#myself';
+    event.preventDefault();
+});
+
+contact.addEventListener('click', function(event){
+    menu.classList.remove('menu-open');
+    window.location.hash='#contact';
+    event.preventDefault();
+});
+
