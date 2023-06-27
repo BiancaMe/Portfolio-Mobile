cf6703dbb175128d9233e29bad3a6e0ef05bb940
BiancaMe <bianquis0410@gmail.com>
2023-06-26 21:32:09 -0300

    Fix linters errors

--- function.js
+++ function.js
@@ -1,36 +1,34 @@
-const action=document.querySelector('#menu-action');
-const menu=document.querySelector('#menu-mobile');
-const close=document.querySelector('.close-menu');
-const headline=document.querySelector('#headline-link');
-const myself=document.querySelector('#myself-link');
-const contact=document.querySelector('#contact-link');
+const action = document.querySelector('#menu-action');
+const menu = document.querySelector('#menu-mobile');
+const close = document.querySelector('.close-menu');
+const headline = document.querySelector('#headline-link');
+const myself = document.querySelector('#myself-link');
+const contact = document.querySelector('#contact-link');
 
-
-action.addEventListener('click', function(event){
-    menu.classList.add('menu-open');
-    event.preventDefault();
+action.addEventListener('click', (event) => {
+  menu.classList.add('menu-open');
+  event.preventDefault();
 });
 
-close.addEventListener('click', function(event){
-    menu.classList.remove('menu-open');
-    event.preventDefault();
+close.addEventListener('click', (event) => {
+  menu.classList.remove('menu-open');
+  event.preventDefault();
 });
 
-headline.addEventListener('click', function(event){
-    menu.classList.remove('menu-open');
-    window.location.hash='#headline';
-    event.preventDefault();
+headline.addEventListener('click', (event) => {
+  menu.classList.remove('menu-open');
+  window.location.hash = '#headline';
+  event.preventDefault();
 });
 
-myself.addEventListener('click', function(event){
-    menu.classList.remove('menu-open');
-    window.location.hash='#myself';
-    event.preventDefault();
+myself.addEventListener('click', (event) => {
+  menu.classList.remove('menu-open');
+  window.location.hash = '#myself';
+  event.preventDefault();
 });
 
-contact.addEventListener('click', function(event){
-    menu.classList.remove('menu-open');
-    window.location.hash='#contact';
-    event.preventDefault();
+contact.addEventListener('click', (event) => {
+  menu.classList.remove('menu-open');
+  window.location.hash = '#contact';
+  event.preventDefault(); 
 });
-
