2d55bc54806846510f647a615ba4f30f62c65e1f
BiancaMe <bianquis0410@gmail.com>
2023-06-27 06:24:44 -0300

    Fix Enlist erros

--- function.js
+++ function.js
@@ -4,14 +4,9 @@ const close = document.querySelector('.close-menu');
 const headline = document.querySelector('#headline-link');
 const myself = document.querySelector('#myself-link');
 const contact = document.querySelector('#contact-link');
-const section = document.getElementsByTagName('section');
-const header = document.querySelector('#headline');
 
 action.addEventListener('click', (event) => {
   menu.classList.add('menu-open');
-  for(let i=0; i<section.length; i++ ) {
-   /* section[i].classList.add('blur');*/
-  }
   event.preventDefault();
 });
 
@@ -37,5 +32,3 @@ contact.addEventListener('click', (event) => {
   window.location.hash = '#contact';
   event.preventDefault();
 });
-
-
