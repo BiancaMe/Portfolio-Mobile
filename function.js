ed3087f7dcaf76fe7c9f42ff6b42d4bf5acc346d
BiancaMe <bianquis0410@gmail.com>
2023-06-28 08:20:41 -0300

    Add PopUp messaje , create popUp.css, update index.html, function.js

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const action = document.querySelector('#menu-action');
 const menu = document.querySelector('#menu-mobile');
 const close = document.querySelector('.close-menu');
@@ -30,5 +31,71 @@ myself.addEventListener('click', (event) => {
 contact.addEventListener('click', (event) => {
   menu.classList.remove('menu-open');
   window.location.hash = '#contact';
+  event.preventDefault();
+});
+
+/* ---- Pop Info Fuction ------ */
+
+function popInfo(event) {
+  event.stopPropagation();
+  const pop = document.querySelector('.popUp');
+  pop.classList.add('popup-visible');
+
+  const card = event.target.parentNode.parentNode.parentNode;
+  const name = card.querySelector('.first-text-section h2').textContent;
+  const desc = card.querySelector('.primary-text').textContent;
+  const img = card.querySelector('.work-presentation-mobile').src;
+  const imgd = card.querySelector('.work-presentation-desktop').src;
+  const tec = card.querySelectorAll('.tags  a');
+  const frame = card.querySelectorAll('.frame-li');
+
+  const obj = {
+    name, desc, img, imgd, tec, frame,
+  };
+
+  const namep = pop.querySelector('.namePop');
+  namep.innerHTML = obj.name;
+
+  const descp = pop.querySelector('.descPop');
+  descp.innerHTML = obj.desc;
+
+  const imgp = pop.querySelector('.img-m-pop');
+  imgp.src = obj.img;
+
+  const imgdesk = pop.querySelector('.img-d-pop');
+  imgdesk.src = obj.imgd;
+
+  const framep = pop.querySelectorAll('.frame-li');
+  for (let i = 0; i < framep.length; i++) {
+    framep[i].innerHTML = obj.frame[i].innerHTML;
+  }
+
+  const tagp = pop.querySelector('.ul-tagPop');
+  for (let i = 0; i < tec.length; i++) {
+    const liPop = document.createElement('li');
+    const aPop = document.createElement('a');
+    const tag = document.createTextNode(tec[i].innerHTML);
+    aPop.appendChild(tag);
+    liPop.appendChild(aPop);
+    tagp.appendChild(liPop);
+  }
+}
+
+/* ------ PopUpCall ----- */
+const detail = document.querySelectorAll('.action a');
+for (let i = 0; i < detail.length; i++) {
+  detail[i].addEventListener('click', popInfo);
+}
+
+/* ----- Close PopUp ----- */
+const closePop = document.querySelector('.close-pop');
+
+closePop.addEventListener('click', (event) => {
+  const pop = document.querySelector('.popUp');
+  pop.classList.remove('popup-visible');
+  const init = pop.querySelectorAll('.ul-tagPop li');
+  for (let i = 0; i < init.length; i++) {
+    init[i].remove();
+  }
   event.preventDefault();
 });
